14c8f925cedcc0fd1a8e006c4497f1977ad83739
Iliyan Iliev <iliyan.hr.iliev@gmail.com>
2024-04-19 19:49:02 +0100

    Create profile views

--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def signup_profile(request):
+    context = {}
+    return render(request, 'accounts/signup_profile.html', context)
+
+
+def signin_profile(request):
+    context = {}
+    return render(request, 'accounts/signin_profile.html', context)
+
+
+def signout_profile(request):
+    return None
+
+
+def details_profile(request):
+    context = {}
+    return render(request, 'accounts/details_profile.html', context)
+
+
+def edit_profile(request):
+    context = {}
+    return render(request, 'accounts/edit_profile.html', context)
+
+
+def delete_profile(request):
+    context = {}
+    return render(request, 'accounts/delete_profile.html', context)
